6b7ca7669f6b5efca30f2cf5be0ed328c63d5fea
Raphael Barreto <tnt.raphael@gmail.com>
2021-05-21 23:46:23 -0300

    Broadcast de logout

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -6,7 +6,7 @@ import { withSSRAuth } from '../utils/withSSRAuth';
 import { Can } from '../components/Can';
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
 
   useEffect(() => {
     api.get('/me').then((response) => console.log(response));
@@ -15,6 +15,9 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
+
+      <button onClick={signOut}>Sign Out</button>
+
       <Can permissions={['metrics.list']}>{<div>Métricas</div>}</Can>
     </>
   );
